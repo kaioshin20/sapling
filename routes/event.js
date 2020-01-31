d08ada33fc9d2503650d2f6a96254f2d93be9e2c
Aayush-1999 <aayushaggarwal2007@gmail.com>
2020-01-31 21:55:32 +0530

    Add request package and develop event post api

--- routes/event.js
+++ routes/event.js
@@ -1,5 +1,6 @@
 const express             = require("express"),
       router              = express.Router(),
+      request             = require("request"),
       Event               = require("../models/event"),
       User                = require("../models/user"),
       middleware          = require("../middleware/verify"),
@@ -17,7 +18,7 @@ router.post("/" ,middleware.isLoggedIn,upload.single("image"),async function(req
         req.body.event.imageId = result.public_id;
         req.body.event.admin = req.user._id;
         let event=await Event.create(req.body.blog);
-        res.redirect("/events");
+        res.redirect(`/event/$(event.id)`);
     } catch(err) {
         req.flash("error", err.message);
         res.redirect("back");
@@ -37,5 +38,40 @@ router.get("/",async function(req,res){
         res.redirect("back");
     }
 })
+
+router.get("/event/:id",(req,res)=>{
+    let event = Event.findById(req.params.id);
+    request(url,(err,response)=>{
+        if(err){
+            console.log(err);
+        }
+        const lat = response.lat;
+        const long = response.long;
+        var options = {
+            'method': 'GET',
+            'url': `https://api.airvisual.com/v2/nearest_city?lat=${lat}&lon=${long}&key=db146a1a-9603-46cc-a580-95abaae0b5d3`,
+            'headers': {
+            }
+        };
+        request(options,(err, response)=> { 
+            if(err){
+                console.log(err);
+            }
+            let data;
+            data.tp=response.data.location.current.weather.tp;
+            data.hu=response.data.location.current.weather.hu;
+            data.ws=response.data.location.current.weather.ws;
+            data.tp=response.data.location.current.weather.tp;
+            data.aqius=response.data.location.current.pollution.aqius;
+            data.mainus=response.data.location.current.pollution.mainus;
+            res.json({event,data});
+        });
+    })
+})
+
+// router.get("/event/:id/interested",(req,res)=>{
+//     let event = Event.findById(req.params.id);
+
+// })
 
 module.exports=router;
